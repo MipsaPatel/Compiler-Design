8c638207edc9a4471ec22140fc08a89886a8b06f
GayathriDeviG <gayathri29.04@gmail.com>
2017-04-17 21:44:03 +0530

    Update ShiftReduceParser.js

--- ShiftReduceParser.js
+++ ShiftReduceParser.js
@@ -49,11 +49,20 @@ function shiftReduceParser() {
 		innerHtml += (stack+"&nbsp;&nbsp;&nbsp;&nbsp"+"&nbsp;&nbsp;&nbsp;&nbsp"+"&nbsp;&nbsp;&nbsp;&nbsp"+str.substring(loc, str.length));
 		for (i=0; i < no; i++)
 		{
-			if (stack=="$"+productions[i][1])
-			{
-				stack = "$"+productions[i][0];
-				break;
-			}
+		if (stack.endsWith(productions[i][1]))
+		{
+			
+			l=productions[i][1].length;
+			
+			stack = stack.substring(0,stack.length-l)+productions[i][0];
+			
+			innerHtml = "&nbsp;&nbsp;&nbsp;&nbsp;";
+			innerHtml += ("STACK"+"&nbsp;&nbsp;&nbsp;&nbsp"+"&nbsp;&nbsp;&nbsp;&nbsp"+"INPUT");
+			innerHtml += ("</br>" + "&nbsp;&nbsp;&nbsp;&nbsp");
+			innerHtml += (stack+"&nbsp;&nbsp;&nbsp;&nbsp"+"&nbsp;&nbsp;&nbsp;&nbsp"+"&nbsp;&nbsp;&nbsp;&nbsp"+str.substring(loc, str.length));
+			//document.write(stack+"&nbsp;&nbsp;&nbsp;&nbsp"+"&nbsp;&nbsp;&nbsp;&nbsp"+"&nbsp;&nbsp;&nbsp;&nbsp"+str.substring(loc, str.length));
+			break;
+		}
 		}
 	}
 	for (i=0; i < no; i++)
